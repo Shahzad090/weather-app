d490231a07d17035954f003fe4e6d4ca6b212ebb
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-07-02 04:14:03 +0100

    uff

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const inputBox = document.getElementsByClassName('input-box');
+const inputBox = document.querySelector('input-box');
 const searchBtn = document.getElementsByid('searchBtn');
-const weather_img = document.getElementsByClassName('weather-img');
-const temprature = document.getElementsByClassName('temprature');
-const discription = document.getElementsByClassName('discription');
+const weather_img = document.querySelector('weather-img');
+const temprature = document.querySelector('temprature');
+const discription = document.querySelector('discription');
 const humidity = document.getElementsByid('humidity');
 const wind_speed = document.getElementsByid('wind-speed');
 
 
 async function checkWeather(city){
-    const api_key = "e55672564eeeb6c1eef9aa0a44a9b185"
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city name} &appid=${api_key}`
+    const api_key = "e55672564eeeb6c1eef9aa0a44a9b185";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
+
+    temprature.innerHTML = `${weather_data.main.temp}`;
 }
 
-console.log(weather_data);
+
 
 searchBtn.addEventListener('click, ()=>'{
     checkWeather(inputBox.value);
