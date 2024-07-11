abb81334f3facc587a22bae06a2d63d4b0ca0dfb
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-07-11 03:50:39 +0100

    ,

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const temprature = document.querySelector(".temprature");
 const discription = document.querySelector(".discription");
 const humidity = document.getElementById("humidity");
 const wind_speed = document.getElementById("wind-speed");
+const location_not_found = document.querySelector("location-not-found");
+const weather_body = document.querySelector("weather-body");
 
 async function checkWeather(city) {
   const api_key = "e55672564eeeb6c1eef9aa0a44a9b185";
@@ -14,33 +16,36 @@ async function checkWeather(city) {
     response.json()
   );
 
-if(weather_data.cod === '404'){
-return;
+  // if (weather_data.cod === "404") {
+  //   location_not_found.style.display = "flex";
+  //   weather_body.style.display = "none";
+  //   console.log("error");
+  //   return;
+  // }
 
-}
-
+  // location_not_found.style.display = "none";
+  // weather_body.style.display = "flex";
   temprature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
-
   discription.innerHTML = `${weather_data.weather[0].main}`;
 
   humidity.innerHTML = `${weather_data.main.humidity}%`;
 
   wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
 
-  switch(weather_data.weather[0].main){
-    case 'Clear':
+  switch (weather_data.weather[0].main) {
+    case "Clear":
       weather_img.src = "/image/clear.png";
       break;
-    case 'Cloud':
+    case "Cloud":
       weather_img.src = "/image/cloud.png";
       break;
-    case 'Mist':
+    case "Mist":
       weather_img.src = "/image/mist.png";
       break;
-    case 'Rain':
+    case "Rain":
       weather_img.src = "/image/rain.png";
       break;
-    case 'Snow':
+    case "Snow":
       weather_img.src = "/image/snow.png";
       break;
   }
